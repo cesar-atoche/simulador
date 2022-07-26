d23ba03520f76f8de3507999e87148fcafcbcc0f
cesar atoche <perumundialito00@gmail.com>
2022-07-26 15:46:56 -0500

    Simulador interactivo

--- js/java.js
+++ js/java.js
@@ -1,15 +1,16 @@
 function numeroNotas() {
     let numero = document.getElementById("numero").value;
+    let nombre = document.getElementById("nombre").value;
     let html = ""
     for (let index = 1; index <= numero; index++) {
         html += `<p>Ingrese Nota ${index} : <input type="text" id="nota${index}"></p>`;
     }
-    html += `<p><input type="button" value="Calcular Promedio" onclick="promedio(${numero})"></p>`;
+    html += `<p><input type="button" value="Calcular Promedio" onclick="promedio(${numero},'${nombre}')"></p>`;
     //html += `<p><input type="reset">`;
     html += `<p id="resultadoPromedio"></p>`;
     document.getElementById("respuesta").innerHTML = html;
 }
-function promedio(numero) {
+function promedio(numero,nombre) {
     let suma = 0;
     let elemento = 0;
     for (let index = 1; index <= numero; index++) {
@@ -25,6 +26,6 @@ function promedio(numero) {
         result = "<b><font color=red>DESAPROBADO</font></b>";
     }
 
-    let html = `Su promedio es ${promedio} y esta ${result}`;
+    let html = `Alumno <b>${nombre.toUpperCase()}</b> tu promedio es ${promedio} y estas ${result}`;
     document.getElementById("resultadoPromedio").innerHTML = html;
 }
